bc23c48d9af1a33a261c33810827741f26bb8da1
Bobby Lin <mr.linjiahao@gmail.com>
2017-06-11 22:28:55 +0800

    Add error handlers in currency-convert

--- currency-convert.js
+++ currency-convert.js
@@ -1,16 +1,27 @@
 const axios = require('axios');
 
-const getExchangeRate = (base, counter) => {
-  return axios.get(`http://api.fixer.io/latest?base=${base}`).then((response) => {
-    return response.data.rates[counter];
-  });
+const getExchangeRate = async (base, counter) => {
+  try {
+    let response = await axios.get(`http://api.fixer.io/latest?base=${base}`);
+    let rate = response.data.rates[counter];
+    if (rate) {
+      return rate;
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    throw new Error(`Unable to get exchange rate for ${base}-${counter} pair.`);
+  }
 };
 
-const getCountries = (currencyCode) => {
-  return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`)
-  .then((response) => {
+const getCountries = async (currencyCode) => {
+  try {
+    let response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
     return response.data.map((country) => country.name);
-  });
+  } catch (e) {
+    throw new Error(`Unable to get list of countries that use ${currencyCode}.`)
+  }
+
 };
 
 const convertCurrency = (base, counter, amount) => {
@@ -26,8 +37,8 @@ const convertCurrency = (base, counter, amount) => {
 };
 
 const convertCurrencyAlt = async (base, counter, amount) => {
-  const countries = await getCountries(counter);
   const rate = await getExchangeRate(base, counter);
+  const countries = await getCountries(counter);
   const exchanged_amt = rate * amount;
   return `${amount} ${base} is worth ${exchanged_amt} ${counter}.\n` +
   `${counter} can be used in the following countries: ${countries}`;
@@ -35,4 +46,6 @@ const convertCurrencyAlt = async (base, counter, amount) => {
 
 convertCurrencyAlt("USD", "SGD", 100000).then((result) => {
   console.log(result);
+}).catch((e) => {
+  console.log(e.message);
 });
